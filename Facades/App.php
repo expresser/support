8ceccfdc468b98ccfa435501cdb1cb8054a60442
Graham Blevins <hgblevins@gmail.com>
2016-09-14 14:36:48 -0400

    replace facade register method with themosis app facade that can be used to register custom service providers

--- Facades/App.php
+++ Facades/App.php
@@ -1,9 +1,9 @@
 <?php namespace Expresser\Support\Facades;
 
-class DB extends \Themosis\Facades\Facade {
+class App extends \Themosis\Facades\Facade {
 
   protected static function getFacadeAccessor() {
 
-    return 'db';
+    return 'app';
   }
 }
